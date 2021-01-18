f5636c5e35ad2d310fd20330b706d2126e67ebeb
Gabriel A. Zorrilla <gabriel@zorrilla.me>
2021-01-18 15:49:00 -0300

    Build .sh

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 # -s -w son para reducir el tamaño del binario, Quita algunas cosas de debug.
 go build -ldflags="-s -w"
 chmod +x microtrack
-#scp csvtodb sistemas@45.33.2.159:/home/sistemas/scripts/microtrack
-#mv csvtodb $HOME/scripts/
+scp microtrack sistemas@45.33.2.159:/home/sistemas/scripts/microtrack
